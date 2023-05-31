cac4dcd94050599e2065d3e65ce89108399f3dcd
Saurabh <saurabhsahofficial@gmail.com>
2023-05-31 21:41:50 +0530

    add try and catch

---
+++ src/controllers/controller.js
@@ -0,0 +1,11 @@
+// const 
+
+
+
+
+//     POST /url/shorten
+// Create a short URL for an original url recieved in the request body.
+// The baseUrl must be the application's baseUrl. Example if the originalUrl is http://abc.com/user/images/name/2 then the shortened url should be http://localhost:3000/xyz
+// Return the shortened unique url.Refer this for the response
+// Ensure the same response is returned for an original url everytime
+// Return HTTP status 400 for an invalid request
