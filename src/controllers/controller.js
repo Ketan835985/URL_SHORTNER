31830d3b1554006bd981ba5aea41cce41351fc87
Kamal Bisht <111355794+kml562@users.noreply.github.com>
2023-06-05 16:15:44 +0530

    done with the branch

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,15 +1,16 @@
 const isUrl = require("is-url");
 const shortId = require("shortid");
 const urlModel = require("../model/urlModel");
-const { GET_ASYNC, SET_ASYNC } = require("../redis/redis");
+const { GET_ASYNC, SET_ASYNC } = require("../utils/redis/redis");
 const axios = require("axios");
 const { response } = require("express");
 
 const isValidUrl = (urlString) => {
-  var urlPattern = new RegExp("(?:https?)://.");
+  var urlPattern = new RegExp("(?:https?)://."); // regx to check https method valid---------------------
   return urlPattern.test(urlString);
 };
 
+// create a new user in the database;-------------------------------------------------------------------
 const createUrlShorten = async (req, res) => {
   try {
     let data = req.body;
@@ -19,77 +20,62 @@ const createUrlShorten = async (req, res) => {
     if (!data.longUrl) {
       return res
         .status(400)
-        .send({ status: false, message: "Please provide a valid URL" });
+        .json({ status: false, message: "Please provide a valid URL" });
     }
-
-    let cachedata = await GET_ASYNC(data.longUrl);
-    //console.log(cachedata);
-    if (cachedata) {
-      // if present then send it to user
-      const caseData = JSON.parse(cachedata);
-      return res.status(200).send({
-        status: true,
-        message: "Already available",
-        caseData,
-      });
-    }
-
+    if (!isValidUrl(data.longUrl))
+      // validation ------------------------------------------
+      return res
+        .status(400)
+        .json({ status: false, message: "Please, Provide valid URL" });
+    if (!isUrl(data.longUrl))
+      // validation ------------------------------------------
+      return res
+        .status(400)
+        .json({ status: false, message: "Please, Provide valid URL" });
+    //finding the data into database----------------------------------------------------------------
     const dbData = await urlModel
       .findOne({ longUrl: data.longUrl })
       .select({ _id: 0, longUrl: 1, shortUrl: 1, urlCode: 1 });
     if (dbData) {
-      return res.status(201).send({ status: true, data: dbData });
+      return res.status(200).json({ status: true, data: dbData });
     } else {
-      let shortCode = shortId.generate();
-      const code = await urlModel.findOne({ urlCode: shortCode });
-      if (code) {
-        shortCode = shortId.generate();
-      }
-      data.urlCode = shortCode;
-      if (!data.longUrl)
-        return res
-          .status(400)
-          .send({ status: false, message: "Please, Provide URL" });
-      if (!isValidUrl(data.longUrl))
-        return res
-          .status(400)
-          .send({ status: false, message: "Please, Provide valid URL" });
-      if (!isUrl(data.longUrl))
-        return res
-          .status(400)
-          .send({ status: false, message: "Please, Provide valid URL" });
-      //console.log("jdkhsdk");
+      // axios for validations--------------------------------------------------------------------
       const response = await axios
         .get(longUrl)
         .then(async (response) => {
-          //     // console.log(res);
-          data.shortUrl = `http://localhost:3000/${data.urlCode}`;
-          let urlCode = data.urlCode;
+          //  short id convert into the lower case string-----------------------------------------------
+          let shortCode = shortId.generate().toLowerCase();
+          const code = await urlModel.findOne({ urlCode: shortCode });
+          if (code) {
+            shortCode = shortId.generate().toLowerCase();
+          }
+          data.urlCode = shortCode;
+          data.shortUrl = `http://${req.headers.host}/${data.urlCode}`;
 
           await urlModel.create(data);
           const saveData = await urlModel
-            .findOne(data)
+            .findOne({ longUrl: data.longUrl })
             .select({ _id: 0, longUrl: 1, shortUrl: 1, urlCode: 1 });
 
-          return res.status(201).send({ status: true, data: saveData });
+          return res.status(201).json({ status: true, data: saveData });
         })
         .catch((err) => {
-          console.log(err);
+          
           return res
             .status(400)
             .json({ status: false, data: "url link in invalid" });
         });
     }
   } catch (error) {
-    //console.log("controller", error);
-    return res.status(500).send({ status: false, message: error.message });
+    console.log("controller", error);
+    return res.status(500).json({ status: false, message: error.message });
   }
 };
 
 const getUrl = async (req, res) => {
   try {
     const fetchFromRedis = await GET_ASYNC(`${req.params.urlCode}`);
-    console.log("hello", fetchFromRedis);
+   
     if (fetchFromRedis) {
       res.status(302).redirect(JSON.parse(fetchFromRedis));
     } else {
@@ -103,11 +89,11 @@ const getUrl = async (req, res) => {
         );
         res.status(302).redirect(url.longUrl);
       } else {
-        res.status(404).send({ status: false, message: "Not found URL" });
+        res.status(404).json({ status: false, message: "Not found URL" });
       }
     }
   } catch (error) {
-    res.status(500).send({ status: false, error: error.message });
+    res.status(500).json({ status: false, error: error.message });
   }
 };
 
